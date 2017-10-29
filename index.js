50a156cbd0510133b59edd82e75d6805755f465a
Kyle Pfahler <pfahlerk1@gmail.com>
2017-10-29 14:22:26 -0400

    cors

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ import googleTrends from 'google-trends-api'
 import Player from './models/player'
 import Query from './models/query'
 
+//  Routes
+import { router } from './routes'
+
+//  Middleware
+const app = express();
 // Cors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-//  Routes
-import { router } from './routes'
-
-//  Middleware
-const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
