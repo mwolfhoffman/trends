8b012076f6dcbc253d5690f10398b678ae5b568c
runnerguy55 <michaelhoffmanj@gmail.com>
2017-11-11 20:53:28 -0700

    refactor api

--- index.js
+++ index.js
@@ -5,27 +5,23 @@ import googleTrends from 'google-trends-api'
 import Player from './models/player'
 import Query from './models/query'
 import { router } from './routes'
-import { defaultErrorHandler } from './utilities/handlers'
-
+import { defaultErrorHandler, corsHandler } from './utilities/handlers'
 
 const app = express();
 
 //  Middleware
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use('*', corsHandler);
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(router);
 
 let totalPlayers = 0;
 
-app.use('/', defaultErrorHandler)
+app.use(defaultErrorHandler)
 
 app.listen('8081')
 console.log('Listening on 8081');
 
-exports = module.exports = app;
+export {
+    app
+}
--- utilities/handlers.js
+++ utilities/handlers.js
@@ -1,29 +1,36 @@
 import app from '../index'
 
+let corsHandler=(req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    return next();
+}
+
 let defaultErrorHandler = (err, req, res, next) => {
-	let error;
-	console.log('Error Caught:')
-	console.log(err);
-	if (res.headersSent) {
-		return next(err);
-	}
-	let env = process.env.NODE_ENV;
-	if (env !== 'production') {
-		error = {
-			ok: false,
-			error: err.message,
-			stack: err
-		}
-	} else {
-		error = {
-			ok: false,
-			error: err.message,
-		}
-	}
-	res.status(400).send(error);
+    let error;
+    console.log('Error Caught:')
+    console.log(err);
+    if (res.headersSent) {
+        return next(err);
+    }
+    let env = process.env.NODE_ENV;
+    if (env !== 'production') {
+        error = {
+            ok: false,
+            error: err.message,
+            stack: err
+        }
+    } else {
+        error = {
+            ok: false,
+            error: err.message,
+        }
+    }
+    res.status(400).send(error);
 }
 
 
 export {
-	defaultErrorHandler
+    corsHandler,
+    defaultErrorHandler
 }
